14cdda9cf64a66f8186bf3903f7748cda83603a2
anrimarhulia-collab <anrimarhulia@gmail.com>
2026-04-27 20:50:03 +0400

    commit

---
+++ group 102/level 2/classwork/lesson2.py
@@ -0,0 +1,20 @@
+
+# 1.
+# saxeli = input("შეიყვანე სახელი: ")
+# gvari = input("შეიყვანე გვარი: ")
+
+# print("user:", saxeli, gvari)
+
+# 2.
+num1 = int(input("შეიყვანე პირველი რიცხვი:"))
+num2 = int(input('შეიყვანე მეორე რიცხვი:'))
+print("+:",num1 + num2)
+print("-:",num1 - num2)
+print("*:",num1 * num2)
+print("/:",num1 / num2)
+print("**:",num1 ** num2)
+print("//:",num1 // num2)
+
+
+
+
